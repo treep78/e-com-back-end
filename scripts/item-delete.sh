6dcc246b910653f2b6188402159117b48fd8262b
treep78 <treep78@gmail.com>
2016-12-16 13:44:34 -0500

    fix redundant file error
    *defined item model in 2 separate files, removed the extra file, wrote a delete curl for items

---
+++ scripts/item-delete.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/items"
+ID=5853fc9ed7c86b252c8d9c01
+TOKEN=RVuIVDmKv1kz/iOv9uYrFYfnS+rGyaH2oh5+0wcUna8=--hbAY9bh2ZYpB/cNO2DykfybkH03WLFnLYdUvl/+Babs=
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request DELETE \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN"
+
+echo
